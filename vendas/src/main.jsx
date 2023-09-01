7e67b87ae085e6d806b3bc3bbe0e75d48ccb32a0
gabsmelin <rm550202@fiap.com.br>
2023-09-01 17:23:14 -0300

    Rotas criadas

--- vendas/src/main.jsx
+++ vendas/src/main.jsx
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//Importando componentes do router-dom
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 
+//importanto páginas
+import Aparelho from './routes/Aparelho.jsx';
+import Erro404 from './routes/Erro404.jsx';
+import Home from './routes/Home.jsx';
+import VizualizarAparelho from './routes/VizualizarAparelho.jsx';
 
+const router = createBrowserRouter([
+  { path: '/', element: <App />, 
+    errorElement: <Erro404 />,
+      children:[
+        { path: '/home', element: <Home />},
+        { path: '/aparelhos', element: <Aparelho />},
+        { path: '/aparelho/aparelhos/:id', element: <VizualizarAparelho />},
+    ]
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
