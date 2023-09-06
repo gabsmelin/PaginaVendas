088cfba4051310e865e39124f8b06f0365f0de3b
gabsmelin <rm550202@fiap.com.br>
2023-09-05 21:42:15 -0300

    Corrigindo roteamento da page Home

--- vendas/src/routes/Home.jsx
+++ vendas/src/routes/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
                 <h1>iPhone 14</h1>
                 <h3>Maravilindo.</h3>
                 <ul>
-                    <li><Link>Saiba Mais</Link></li>
+                    <li><Link to='/aparelho'>Saiba Mais</Link></li>
                     <li><Link>Comprar</Link></li>
                 </ul>
                 
